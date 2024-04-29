d950c2b142b2fcc970a355b96f73ebc015eebfb4
im-victor-mendez <106124660+im-victor-mendez@users.noreply.github.com>
2024-04-28 20:54:51 -0600

    ci: Added hamburger and sandwich ingredients

---
+++ database/ingredients.sql
@@ -0,0 +1,39 @@
+DELETE FROM `ingredient`;
+-- Fruits
+INSERT INTO `ingredient`(`name`, `unit_weight`)
+VALUES ("Piña", 2000),
+    ("Aguacate", 200);
+-- Vegetables
+INSERT INTO `ingredient`(`name`, `unit_weight`)
+VALUES ("Jitomate", 150),
+    ("Lechuga", 500),
+    ("Cebolla", 400),
+    ("Chiles en Vinagre", 1000),
+    ("Papa", 40);
+-- -- Mushrooms
+-- INSERT INTO `ingredient`(`name`, `unit_weight`);
+-- Meats
+INSERT INTO `ingredient`(`name`, `unit_weight`)
+VALUES ("Salchicha", 70),
+    ("Jamón", 20),
+    ("Carne para hamburguesa", 150),
+    ("Pechuga de pollo", 200);
+-- Dairy
+INSERT INTO `ingredient`(`name`, `unit_weight`)
+VALUES ("Queso Oaxaca", 1000),
+    ("Queso amarillo", 28),
+    ("Queso panela", 1000),
+    ("Queso manchego", 400);
+-- -- Fish
+-- INSERT INTO `ingredient`(`name`, `unit_weight`);
+-- Condiments
+INSERT INTO `ingredient`(`name`, `unit_weight`)
+VALUES ("Mostaza", 1000),
+    ("Catsup", 1000),
+    ("Mayonesa", 1000),
+    ("Salsa Chipotle", 1000);
+-- Bread
+INSERT INTO `ingredient`(`name`, `unit_weight`)
+VALUES ("Pan para hamburguesa", 100),
+    ("Pan blanco", 250),
+    ("Pan integral", 250);
