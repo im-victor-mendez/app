ea435a02788c6a0c1154ad8905056ccaf0294ddc
im-victor-mendez <106124660+im-victor-mendez@users.noreply.github.com>
2024-05-06 20:45:11 -0600

    ci: Added definition of ingredients endpoint

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import recipes from './api/recipes';
+import ingredients from './api/ingredients';
 
 export default (app: Express) => {
 	app.use('/api/recipes', recipes);
+	app.use('/api/ingredients', ingredients);
 };
