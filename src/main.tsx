5ec92ab874e6dd1c223b1fb87501e4c7ff031f6f
im-victor-mendez <106124660+im-victor-mendez@users.noreply.github.com>
2024-05-19 21:09:56 -0600

    feat: Added routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Inventory from './inventory/Inventory.tsx';
-import OrderScreen from './order/OrderScreen.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Inventory from './inventory/Inventory';
+import OrderScreen from './order/OrderScreen';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		{/* To replace with App properly */}
-		<OrderScreen />
+		<BrowserRouter>
+			<Routes>
+				<Route
+					key={`Inventory-Route`}
+					path={'/inventory'}
+					element={<Inventory />}
+				/>
+				<Route key={`Order-Route`} path={'/orders'} element={<OrderScreen />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
