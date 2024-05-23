ef62010acb88fe0dc1979e373c1ee938a2345c3c
im-victor-mendez <106124660+im-victor-mendez@users.noreply.github.com>
2024-05-22 20:33:24 -0600

    feat: Added something???

--- src/inventory/Inventory_Ingredient.tsx
+++ src/inventory/Inventory_Ingredient.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { fetchData } from '../api/data';
 import InventoryEntity from './Inventory_Entity';
 
 /**
@@ -10,6 +12,16 @@ function InventoryIngredient(entity: InventoryEntity) {
 
 	const isCritical = quantity <= warning_quantity;
 	const isWarning = quantity <= warning_quantity * 1.1;
+
+	const [ingredientUsage, setIngredientUsage] = useState(0);
+
+	useEffect(() => {
+		async function getData() {
+			const data = await getIngredientUsage(name);
+			setIngredientUsage(data);
+		}
+		getData();
+	}, []);
 
 	return (
 		<div
@@ -24,11 +36,22 @@ function InventoryIngredient(entity: InventoryEntity) {
 				<p className='warning_quantity'>
 					Cantidad de Riesgo: {warning_quantity}
 				</p>
+				<p className='usage_quantity'>
+					Cantidad que se Utiliza en Total: {ingredientUsage}
+				</p>
 			</div>
 
 			<p className='total_weight'>Peso total: {total_weight} g</p>
 		</div>
 	);
+}
+
+async function getIngredientUsage(name: string): Promise<number> {
+	const data = (await fetchData(`/ingredients/usage/${name}`)).data[0][
+		'total_usage'
+	];
+	console.log(data);
+	return data;
 }
 
 export default InventoryIngredient;
